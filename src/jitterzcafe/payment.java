5de214a27678929d70f91f13c3119cc2635f1228
jayrogers <jannalrgrs@gmail.com>
2016-08-15 16:23:44 -0400

    testing add

--- src/jitterzcafe/payment.java
+++ src/jitterzcafe/payment.java
@@ -1,6 +1,69 @@
 package jitterzcafe;
 
+public class payment extends Product{
 
-public class payment extends Product {
+
+	
+	private double quantity;
+	private double salesTax;
+	private String paymentType; 
+
+	
+	
+	public payment(String name, String category, String description, double price, double quantity, double salesTax,
+			String paymentType) {
+		super(name, category, description, price);
+		setQuantity(quantity);
+		setSalesTax(salesTax);
+		setPaymentType(paymentType);
+	}
+
+
+	//public payment(String name, String category, String description, double price) {
+		//super(name, category, description, price);
+	//}
+	
+
+	public double getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public double getSalesTax() {
+		return salesTax;
+	}
+
+
+	public void setSalesTax(double salesTax) {
+		this.salesTax = salesTax;
+	}
+
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+
+	public double getSubTotal(){
+		double subtotal = 0;
+		return subtotal;
+	}
+	public double getGrandTotal(){
+		double grandtotal = 0;
+		return grandtotal;
+	}
+
+
+	
 
 }
