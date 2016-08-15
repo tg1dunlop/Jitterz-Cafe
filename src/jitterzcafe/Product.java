91bb86982445434e04bec7210ac9291eca0a21df
jayrogers <jannalrgrs@gmail.com>
2016-08-15 15:06:50 -0400

    "Add Product Methods"

--- src/jitterzcafe/Product.java
+++ src/jitterzcafe/Product.java
@@ -1,5 +1,51 @@
 package jitterzcafe;
 
 public class Product {
+	private String name;
+	private String category;
+	private String description;
+	private double price;
+	
+
+	public Product(String name, String category, String description, double price) {
+		super();
+		setName(name);
+		setCategory(category);
+		setDescription(description);
+		setPrice(price);
+	}
+	
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
