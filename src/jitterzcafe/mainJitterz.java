5b84c966575960fea5b1b00f8b71479c7938b6a0
tg1dunlop <tracy.dunlop@gmail.com>
2016-08-15 16:33:24 -0400

    merge conflicts

--- src/jitterzcafe/mainJitterz.java
+++ src/jitterzcafe/mainJitterz.java
@@ -1,10 +1,19 @@
 package jitterzcafe;
 
+import java.util.Scanner;
+
 public class mainJitterz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+Scanner scan1 = new Scanner(System.in);
+int userInput = 0;
+System.out.println("Welcome to Jitterz-Cafe!");
+//display menu
+System.out.println("Menu: Coffee - enter 1, Muffin - enter 2");
+System.out.println("What would you like to order?  Please enter the menu item number: ");
+userInput = scan1.nextInt();
+scan1.nextLine();
+System.out.println("You entered: " + userInput);
 	}
 
 }
--- src/jitterzcafe/payment.java
+++ src/jitterzcafe/payment.java
@@ -1,6 +1,65 @@
 package jitterzcafe;
 
-
 public class payment extends Product {
+	
+	private double quantity;
+	private double salesTax;
+	private String paymentType;
+	private String checkNum;
+	
+	//public payment(String name, String category, String description, double price) {
+		//super(name, category, description, price);
+//}
+	
+	private payment(String name, String category, String description, double price, double quantity, double salesTax,
+			String paymentType, String checkNum) {
+		super(name, category, description, price);
+		setQuantity(quantity);
+		setSalesTax(salesTax);
+		setPaymentType(paymentType);
+		setCheckNum(checkNum);
+	}
 
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getSalesTax() {
+		return salesTax;
+	}
+
+	public void setSalesTax(double salesTax) {
+		this.salesTax = salesTax;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+	//put in extra methods for the math 
+	public double getSubTotal(){
+		double subtotal = 0;
+		return subtotal;
+	}
+	public double getGrandTotal() {
+	double grandTotal = 0;
+	return grandTotal; 
+	}
+//from getters/setters
+	public String getCheckNum() {
+		return checkNum;
+	}
+
+	public void setCheckNum(String checkNum) {
+		this.checkNum = checkNum;
+	}
+	
+	
 }
