33b75060bd354b33686f87ec6dc86666007d31d5
tg1dunlop <tracy.dunlop@gmail.com>
2016-08-17 10:39:29 -0400

    Restructured main method.

--- src/jitterzcafe/mainJitterz.java
+++ src/jitterzcafe/mainJitterz.java
@@ -3,7 +3,10 @@ package jitterzcafe;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class mainJitterz {
 
@@ -11,167 +14,148 @@ public class mainJitterz {
 
 		Scanner scan1 = new Scanner(System.in);
 		String userName = "";
-		//int userInput = 0;
-		int userSelection = 1;
+		int userInput = 0;
+		// int userSelection = 1;
 		String answer = "y";
-		System.out.println("Welcome to Jitterz-Cafe!");
+		double stotal = 0;
+
+		System.out.println("Welcome to Jitterz Cafe!");
 		System.out.println("May I have your name? ");
 		userName = scan1.nextLine(); // store name
 
-		/*
-		 * Path filePath = Paths.get("menuItems.txt"); File menuFile =
-		 * filePath.toFile();
-		 */
-
-		// displaying the menu from MyProduct
-		// MyProduct.writeToFile(menuFile);
-		// MyProduct.readFile(menuFile);
-
-		/*
-		 * System.out.
-		 * println("What would you like to order?  Please enter the menu item number: "
-		 * ); System.out.println("Press category");
-		 * System.out.println("Press description");
-		 * System.out.println("Press price"); userInput = scan1.nextInt();
-		 * scan1.nextLine();
-		 */
-
-		// create while loop for categories
-		while (answer.equalsIgnoreCase("y")) {
+		ArrayList<Product> prodlist = new ArrayList<Product>();
+		// Category, Description, Menu item, price
+		prodlist.add(new Product(userName, "Coffee", "Iced Coffee", "a", 3.50));
+		prodlist.add(new Product(userName, "Coffee", "Decaf Coffee", "b", 2.50));
+		prodlist.add(new Product(userName, "Coffee", "Regular Coffee", "c", 2.00));
+		prodlist.add(new Product(userName, "Lattes", "Regular", "r", 3.50));
+		prodlist.add(new Product(userName, "Lattes", "Flavored", "f", 2.50));
+		prodlist.add(new Product(userName, "Lattes", "Decaf Mocha", "c", 2.00));
+		prodlist.add(new Product(userName, "Pastries", "Beignet", "bg", 3.50));
+		prodlist.add(new Product(userName, "Pastries", "Scone", "s", 2.50));
+		prodlist.add(new Product(userName, "Pastries", "Muffin", "m", 1.00));
+		prodlist.add(new Product(userName, "After Hours", "Baileys", "y", 4.50));
+		prodlist.add(new Product(userName, "After Hours", "Kahula", "k", 5.50));
+		prodlist.add(new Product(userName, "After Hours", "Whiskey", "w", 9.00));
+		
+		while (choice.equalsIgnoreCase("y")) {
 			
-			System.out.println("Select '1' to order Coffee.");
-			System.out.println("Select '2' to order Lattes.");
-			System.out.println("Select '3' to order Pastries.");
-			System.out.println("Select '4' for after hours specials!!");
-			System.out.println("Select '0' to exit.");
-			System.out.println();
-			System.out.print("Please enter your selection: ");
-			userSelection = scan1.nextInt();
+			displayMenu();
+			
+			System.out.println(userName + " What would you like to order today? Please enter item number from the menu:");
+			userInput = scan1.nextInt();
 			scan1.nextLine();
-
-			// begin array formation
-			Product[] orderArray = new Product[12];
-
-			// create switch case for descriptions of chosen category
-			switch (userSelection) {
-
+			int qty = 0;
+			payment ct = new payment(qty, .06, "cash");
+			
+			switch (userInput) 
+			{
 			case 1:
 				System.out.println("Please enter a for Iced coffee. b for Decaf. c for Regular");
-				String coffeeChoice1 = scan1.nextLine();
-
-				getCoffee(userName, orderArray, coffeeChoice1);
+				String Choice1 = scan1.nextLine();
+				System.out.println("How many would you like? ");
+				qty = scan1.nextInt();
+				scan1.nextLine();
+				stotal = displayCurTotal(scan1, ct, qty, stotal, prodlist, Choice1);
 				break;
-
-			case 2: {
-				System.out.println("Please enter a for Regular. b for Flavored. c for Decaf Mocha");
-				String coffeeChoice2 = scan1.nextLine();
-
-				getLattes(userName, orderArray, coffeeChoice2);
+			case 2: 
+				System.out.println("Please enter r for Regular.  f for Flavored. dm for Decaf Mocha");
+				 Choice1 = scan1.nextLine();
+				 System.out.println("How many would you like? ");
+					qty = scan1.nextInt();
+					scan1.nextLine();
+				 stotal = displayCurTotal(scan1, ct, qty, stotal, prodlist, Choice1);
+				 break;
+			case 3: 
+				System.out.println("Please enter bg for Beignet. s for Scone. m for Muffin");
+				Choice1 = scan1.nextLine();
+				System.out.println("How many would you like? ");
+				qty = scan1.nextInt();
+				scan1.nextLine();
+				 stotal = displayCurTotal(scan1, ct, qty, stotal, prodlist, Choice1);
 				break;
-			}
+			case 4: 
+				System.out.println("Please enter y for Baileys. k for Kahlua. w for Whiskey");
+				Choice1 = scan1.nextLine();
+				System.out.println("How many would you like? ");
+				qty = scan1.nextInt();
+				scan1.nextLine();
+				 stotal = displayCurTotal(scan1, ct, qty, stotal, prodlist, Choice1);
+				break;
 			
-			case 3: {
-				System.out.println("Please enter a for Beignet. b for Scone. c for Muffin");
-				String coffeeChoice3 = scan1.nextLine();
-
-				getPastries(userName, orderArray, coffeeChoice3);
-				break;
-			}
-
-			case 4: {
-				System.out.println("Please enter a for Baileys. b for Kahlua. c for Whiskey");
-				String coffeeChoice4 = scan1.nextLine();
-
-				getHappyHours(userName, orderArray, coffeeChoice4);
-				
-				System.out.println("Would you like to order something else? y/n: ");
-				answer = scan1.nextLine();
-				break;
-			}
-
 			default:
-				System.out.println("Goodbye");
 				break;
-			}// end of switch
-		} // end of while*/
-		scan1.close();
+			} // end of switch
+			
+			System.out.println("Would you like to add to this order? Y or N");
+			answer = scan1.nextLine();
+			System.out.println("What is choice: " + answer);
+		}// end
+																																				// of
+																																				// while
 
-	}
+	public static double displayCurTotal(Scanner scan1, payment ct, qty, double stotal, ArrayList<Product> prodlist,
+				String Choice1)) {
+			
+			
+			for (int i = 0; i < prodlist.size(); i++)  {
+				if((prodlist.get(i).getMenuItem().equals(Choice1))) {
+					
+					switch(Choice1) {
+					case "a":
+						System.out.println("You have ordered " +qty+ "Iced Coffee.");
+						break;
+					case "b":
+						System.out.println("You have ordered "+qty+ " Decaf Coffee.");
+						break;
+					case "c":
+						System.out.println("You have ordered "+qty+ " Regular Coffee.");
+						break;
+					case "r":
+						System.out.println("You have ordered "+qty+ " Regular Latte");
+						break;
+					case "f":
+						System.out.println("You have ordered "+qty+ " Flavored Latte");
+						break;
+					case "dm":
+						System.out.println("You have ordered "+qty+ " Decaf Mocha Latte");
+						break;
+					case "bg":
+						System.out.println("You have ordered "+qty+ " Beignet");
+						break;
+					case "s":
+						System.out.println("You have ordered "+qty+ " Scone");
+						break;
+					case "m":
+						System.out.println("You have ordered "+qty+ " Muffin");
+						break;
+					case "y":
+						System.out.println("You have ordered "+qty+ " Baileys");
+						break;
+					case "k":
+						System.out.println("You have ordered "+qty+ " Kahula");
+						break;
+					case "w":
+						System.out.println("You have ordered "+qty+ " Whiskey");
+						break;
+					}
+					System.out.println("Your subtotal is: " +stotal);
+				}//end switch
+				
+			}//end for loop
+			return stotal;
+			
+			}
 
-	public static void getHappyHours(String userName, Product[] orderArray, String coffeeChoice4) {
-		if (coffeeChoice4.equalsIgnoreCase("a")) {
-			Product p10 = new Product(userName, "after hours", "baileys", 1.00);
-			System.out.println(p10.getName());
-			orderArray[9] = p10;
+	public static void displayMenu() {
+		// set up file paths
+		Path filePath = Paths.get("menuItems.txt");
+		File menuFile = filePath.toFile();
+		// displaying the menu from MyProduct
+		MyProduct.writeToFile(menuFile);
+		MyProduct.readFile(menuFile);
 		}
-		if (coffeeChoice4.equalsIgnoreCase("b")) {
-			Product p11 = new Product(userName, "after hours", "kahlua", 1.00);
-			System.out.println(p11.getName());
-			orderArray[10] = p11;
-		}
-		if (coffeeChoice4.equalsIgnoreCase("c")) {
-			Product p12 = new Product(userName, "after hours", "whiskey", 1.00);
-			System.out.println(p12.getName());
-			orderArray[11] = p12;
-		}
-	}
-
-	public static void getPastries(String userName, Product[] orderArray, String coffeeChoice3) {
-		if (coffeeChoice3.equalsIgnoreCase("a")) {
-			Product p7 = new Product(userName, "pastries", "beignet", 1.00);
-			System.out.println(p7.getName());
-			orderArray[6] = p7;
-		}
-		if (coffeeChoice3.equalsIgnoreCase("b")) {
-			Product p8 = new Product(userName, "pastries", "Scone", 1.00);
-			System.out.println(p8.getName());
-			orderArray[7] = p8;
-		}
-		if (coffeeChoice3.equalsIgnoreCase("c")) {
-			Product p9 = new Product(userName, "pastries", "muffin", 1.00);
-			System.out.println(p9.getName());
-			orderArray[8] = p9;
-		}
-	}
-
-	public static void getLattes(String userName, Product[] orderArray, String coffeeChoice2) {
-		if (coffeeChoice2.equalsIgnoreCase("a")) {
-			Product p4 = new Product(userName, "lattes", "regular", 1.00);
-			System.out.println(p4.getName());
-			orderArray[3] = p4;
-			//check if array is working
-			// System.out.println(orderArray[0].getName());
-			// System.out.println(orderArray[0].getPrice());
 
 		}
-		if (coffeeChoice2.equalsIgnoreCase("b")) {
-			Product p5 = new Product(userName, "lattes", "flavored", 1.00);
-			System.out.println(p5.getName());
-			orderArray[4] = p5;
-		}
-		if (coffeeChoice2.equalsIgnoreCase("c")) {
-			Product p6 = new Product(userName, "lattes", "decaf mocha", 1.00);
-			System.out.println(p6.getName());
-			orderArray[5] = p6;
-		}
-	}
 
-	public static void getCoffee(String userName, Product[] orderArray, String coffeeChoice1) {
-		if (coffeeChoice1.equalsIgnoreCase("a")) {
-			Product p1 = new Product(userName, "Coffee", "Iced Coffee", 1.00);
-			System.out.println(p1.getName());
-			orderArray[0] = p1;
-		}
-		if (coffeeChoice1.equalsIgnoreCase("b")) {
-			Product p2 = new Product(userName, "Coffee", "decaf", 1.00);
-			System.out.println(p2.getName());
-			orderArray[1] = p2;
-		}
-		if (coffeeChoice1.equalsIgnoreCase("c")) {
-			Product p3 = new Product(userName, "Coffee", "regular", 1.00);
-			System.out.println(p3.getName());
-			orderArray[2] = p3;
-		}
-	}
-}// end of main
-
+}
